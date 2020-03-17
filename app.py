590362ec932eb701bb8ed3977ea240e361e1281e
jsmzdf <sebastianjhon9427@gmail.com>
2020-03-17 07:59:34 -0500

    se agregan mas cosas

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/data',methods=['POST'])
+def data():
+    if request.method == 'POST':
+        archivoCSV = pd.DataFrame(request.form['index.html'])
+        return 'llego el archivo'
+
+@app.route('/InfoData')
+def infodata():
+    return 'info data'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
